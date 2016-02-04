871d56c179ab76fa003d72672fee3ffc8c2baa6a
Joel Kortelainen <jokke.kortelainen@gmail.com>
2016-02-04 19:17:56 +0200

    Tehtävä 4 valmis

--- IIO11300Vktehtavat/Tehtava4/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava4/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Tehtava4
 {
     /// <summary>
@@ -20,9 +21,130 @@ namespace Tehtava4
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Pelaaja> pelaajat;
         public MainWindow()
         {
             InitializeComponent();
+            string[] seurat = new string[] { "Ilves", "Tappara", "JYP", "Kärpät", "Blues", "Ässät", "TPS", "HJK" };
+            pelaajat = new List<Pelaaja>();
+            cbSeura.ItemsSource = seurat;
+        }
+
+        private void btnUusi_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txtEtunimi.Text) &&
+                       !string.IsNullOrWhiteSpace(txtSukunimi.Text) &&
+                       !string.IsNullOrWhiteSpace(txtHinta.Text) &&
+                       cbSeura.SelectedItem != null)
+                {
+                    Pelaaja pelaaja = new Pelaaja(txtEtunimi.Text,
+                                                    txtSukunimi.Text,
+                                                    cbSeura.SelectedValue.ToString(),
+                                                    int.Parse(txtHinta.Text));
+                    bool containsPlayer = pelaajat.Any(Pelaaja => Pelaaja.KokoNimi == pelaaja.KokoNimi);
+                    if(containsPlayer)
+                    {
+                        pelaaja = null;
+                        txtStatus.Text = "Pelaaja on jo listassa";                     
+                    }
+                    else
+                    {
+                        pelaajat.Add(pelaaja);
+                        ApplyChanges();
+                        txtStatus.Text = "Uusi pelaaja luotu";
+                    }                 
+                }
+                else
+                {
+                    txtStatus.Text = "Täytä kaikki kentät";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.ToString();
+            }
+        }
+
+        private void btnTallenna_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int selectedIndex = lbPelaajat.Items.IndexOf(lbPelaajat.SelectedItem);
+                pelaajat[selectedIndex].Etunimi = txtEtunimi.Text;
+                pelaajat[selectedIndex].Sukunimi = txtSukunimi.Text;
+                pelaajat[selectedIndex].Seura = cbSeura.SelectedValue.ToString();
+                pelaajat[selectedIndex].Hinta = int.Parse(txtHinta.Text);
+                Clear();
+                ApplyChanges();
+                txtStatus.Text = "Pelaaja " + pelaajat[selectedIndex].KokoNimi + " tallennettu";
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.ToString();
+            }
+        }
+        private void Clear()
+        {
+            txtEtunimi.Text = "";
+            txtSukunimi.Text = "";
+            txtHinta.Text = "";
+            cbSeura.SelectedValue = "";
+        }
+        private void btnPoista_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                pelaajat.RemoveAt(lbPelaajat.Items.IndexOf(lbPelaajat.SelectedItem));
+                txtStatus.Text = "Pelaaja poistettu";
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.ToString();
+            }
+            Clear();
+            ApplyChanges();
+        }
+
+        private void btnKirjoita_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Pelaaja.SaveToFile(pelaajat);
+                txtStatus.Text = "Pelaajat kirjoitettu tiedostoon";
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.ToString();
+            }
+        }
+
+        private void btnLopeta_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void ApplyChanges()
+        {
+            lbPelaajat.ItemsSource = null;
+            lbPelaajat.ItemsSource = pelaajat;
+        }
+
+        private void lbSelection_Click(object sender, SelectionChangedEventArgs e)
+        {
+            if(lbPelaajat.SelectedItem != null)
+            {
+                int selectedIndex = lbPelaajat.Items.IndexOf(lbPelaajat.SelectedItem);
+                txtEtunimi.Text = pelaajat[selectedIndex].Etunimi;
+                txtSukunimi.Text = pelaajat[selectedIndex].Sukunimi;
+                txtHinta.Text = pelaajat[selectedIndex].Hinta.ToString();
+                cbSeura.SelectedValue = pelaajat[selectedIndex].Seura;
+                txtStatus.Text = "Valittu " + pelaajat[selectedIndex].KokoNimi;
+            }
+            else
+            {
+                txtStatus.Text = "Valinta on tyhjä";
+            }
         }
     }
 }
---
+++ IIO11300Vktehtavat/Tehtava4/Pelaaja.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tehtava4
+{
+    class Pelaaja
+    {
+        #region Variables
+        private string etunimi;
+        private string sukunimi;
+        private string seura;
+        private int hinta;
+        #endregion
+        #region Properties
+        public string Etunimi
+        {
+            get
+            {
+                return etunimi;
+            }
+            set
+            {
+                etunimi = value;
+            }
+        }
+        public string Sukunimi
+        {
+            get
+            {
+                return sukunimi;
+            }
+            set
+            {
+                sukunimi = value;
+            }
+        }
+        public string Seura
+        {
+            get
+            {
+                return seura;
+            }
+            set
+            {
+                seura = value;
+            }
+        }
+        public int Hinta
+        {
+            get
+            {
+                return hinta;
+            }
+            set
+            {
+                hinta = value;
+            }
+        }
+        public string KokoNimi
+        {
+            get
+            {
+                return (etunimi + " " + sukunimi);
+            }
+        }
+        public string EsitysNimi
+        {
+            get
+            {
+                return (KokoNimi + ", " + seura);
+            }
+        }
+        #endregion
+        #region Constructors
+        public Pelaaja(string etu, string suku, string seura, int hinta)
+        {
+            this.etunimi = etu;
+            this.sukunimi = suku;
+            this.seura = seura;
+            this.hinta = hinta;
+        }
+        #endregion
+        #region Methods
+        public static void SaveToFile(List<Pelaaja> pelaajat)
+        {
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Pelaajat";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                StreamWriter writer = File.AppendText(dialog.FileName);
+                foreach(var pelaaja in pelaajat)
+                {
+                    writer.WriteLine(pelaaja);
+                }
+                writer.Close();
+            }
+        }
+        public override string ToString()
+        {
+            return EsitysNimi;
+        }
+        #endregion
+
+
+    }
+}
