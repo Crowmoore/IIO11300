87370e5ee82e2df98c338942b7020981c9f56307
Crowmoore <jokke.kortelainen@gmail.com>
2016-03-10 09:43:20 +0200

    Databinding continued

--- H6DataBinding/Hockey.cs
+++ H6DataBinding/Hockey.cs
@@ -1,14 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace H6DataBinding
 {
-    public class HockeyPlayer
+    public static class TestHockeyBench
     {
+        public static List<HockeyPlayer> Get3Players()
+        {
+            List<HockeyPlayer> players = new List<HockeyPlayer>();
+            players.Add(new HockeyPlayer("Teemu Selänne", "8"));
+            players.Add(new HockeyPlayer("Jarkko Immonen", "26"));
+            players.Add(new HockeyPlayer("Ville Peltonen", "16"));
+            return players;
+        }
+    }
+    public class HockeyPlayer : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                Notify("Name");
+                Notify("NameAndNumber");
+            }
+        }
+        private string number;
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+                Notify("Number");
+                Notify("NameAndNumber");
+            }
+        }
+        public string NameAndNumber
+        {
+            get
+            {
+                return name + "#" + number;
+            }
+        }
 
+        public HockeyPlayer()
+        {
+
+        }
+
+        public HockeyPlayer(string name, string number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+        //INotifyPropertyChanged (interface) implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
     }
 
     public class HockeyTeam
